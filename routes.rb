c9fb217366a57e369ea7069ccb5a9df55a68db2d
soutchay <chungvus@gmail.com>
2014-12-04 10:43:24 -0800

    def username for username test

--- routes.rb
+++ routes.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+def username_for(firstname, lastname)
+	firstname[0] + lastname[0,5]
+end
+
 get '/' do
 	erb :home_page #tells Sinatra to render views/home_page.erb
 end
@@ -11,6 +15,18 @@ post '/cart' do
 	erb :cart
 end
 
-get '/:lastname/:firstname' do
-	"Alfonso Rush's username is arush"
+get "/:lastname/:firstname" do
+    firstname = params[:firstname]
+    lastname = params[:lastname]
+    username = username_for(firstname, lastname).downcase
+    "#{firstname} #{lastname}'s username is #{username}"
+end
+
+get '/:lastname/:firstname/:middlename' do
+    firstname = params[:firstname]
+    lastname = params[:lastname]
+    middlename = params[:middlename]
+    username = username_for(firstname, middlename, lastname)
+    "#{firstname} #{lastname}'s username is #{username}"
+	"Alfonso D Rush's username is adrush"
 end
