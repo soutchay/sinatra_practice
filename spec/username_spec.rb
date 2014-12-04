b2afee3e0655ff4c52102b5bcf371987178f51fa
soutchay <chungvus@gmail.com>
2014-12-04 10:25:44 -0800

    add tests

---
+++ spec/username_spec.rb
@@ -0,0 +1,28 @@
+require File.dirname(__FILE__) + '/../routes'
+require 'rack/test'
+
+set :environment, :test
+
+describe 'The HelloWorld App' do
+	include Rack::Test::Methods
+
+	def app
+		Sinatra::Application
+	end
+	context 'get "/Rush/Alfonso"' do
+		before {get '/Rush/Alfonso' }
+		specify {expect(last_respones).to be_ok }
+		specify {expect(last_response.body).to include "Alfonso Rush's username is arush" }
+	end
+  # context 'get "/Rush/Alfonso/D"' do
+  #   before { get '/Rush/Alfonso/D' }
+  #   specify { expect(last_response).to be_ok }
+  #   specify { expect(last_response.body).to include "Alfonso D Rush's username is adrush" }    
+  # end
+
+  # context 'get "/Matsumoto/Yukihiro"' do
+  #   before { get '/Matsumoto/Yukihiro' }
+  #   specify { expect(last_response).to be_ok }
+  #   specify { expect(last_response.body).to include "Yukihiro Matsumoto's username is ymatsu" }
+  # end
+end
